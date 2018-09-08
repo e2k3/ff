afcf7abc2c8bb54e8d92b66c24dab6633c850406
e2k3 <43041483+e2k3@users.noreply.github.com>
2018-09-08 19:19:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -863,6 +863,11 @@ client.on('message', message => {
 
 
 
+client.on('message', msg => {
+  if(msg.content === '.')
+  msg.channel.send('**** Welcome To __Speical Community__ :champagne_glass: . ****')
+});
+
 
 
 
