048bf67ae27bbdba1b44a95dceba43f1c3e3f9da
e2k3 <43041483+e2k3@users.noreply.github.com>
2018-09-08 21:35:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -926,6 +926,24 @@ client.on('message', msg => {
 
 
 
+         client.on('message', message => {
+            if (message.content.startsWith(prefix + "bot")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField(' السيرفرات🌐',`[${client.guilds.size}]  `)
+.addField(' الاعضاء👥 ',` [${client.users.size}] `)
+.addField('الرومات📚 ',`[${client.channels.size}]`) 
+.addField(' البنق🚀 ',`[${Date.now() - message.createdTimestamp}]`) 
+.addField('مصمم  + صاحب البوت ',`اسمك هنا`)
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
